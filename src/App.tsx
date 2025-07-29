0df740f16d0f62124c126741c2cd5dcc86544575
t.nakamura <tikuwa.fish@gmail.com>
2025-07-29 11:37:43 +0900

    読み込み時にセッションを取得する

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,35 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TodoPage from "./pages/todo/TodoPage";
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
+import { useCurrentUser } from "./modules/auth/current-user.state";
+import { authRepository } from "./modules/auth/auth.repository";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const currentUserStore = useCurrentUser();
+
+  const setSession = async () => {
+    try {
+      const currentUser = await authRepository.getCurrentSession();
+      if (currentUser) {
+        currentUserStore.set(currentUser);
+      }
+    } catch (error) {
+      console.error("Error setting session:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setSession();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>; // ローディング中の表示
+  }
+
   return (
     <BrowserRouter>
       <Routes>
